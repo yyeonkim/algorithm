d84522325df0823f1ffcf6976da5812f2a529696
yyeonkim <seyeon.dev@gmail.com>
2023-11-24 00:13:17 +0900

    출처 표시

--- PriorityQueue/Heap.js
+++ PriorityQueue/Heap.js
@@ -1,3 +1,5 @@
+// 출처: https://jun-choi-4928.medium.com/javascript%EB%A1%9C-heap-priority-queue-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0-8bc13bf095d9
+
 /* 최소 힙 */
 class Heap {
   constructor() {
--- PriorityQueue/PriorityQueue.js
+++ PriorityQueue/PriorityQueue.js
@@ -1,3 +1,5 @@
+// 출처: https://jun-choi-4928.medium.com/javascript%EB%A1%9C-heap-priority-queue-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0-8bc13bf095d9
+
 const Heap = require("./Heap.js");
 
 class PriorityQueue extends Heap {
